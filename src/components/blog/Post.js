343955a5178311e927cb0f67f5acdb4f080b95b3
conceptdecorators <conceptdecorators@email.com>
2021-07-26 14:47:27 -0400

    first react depoly with heroku test

--- src/components/blog/Post.js
+++ src/components/blog/Post.js
@@ -5,17 +5,33 @@ const Post = ({ article }) => {
   console.log(article);
   const { name, featuredImage, description } = article.fields;
   const postDescription = marked(description);
+  const { url, contentType, fileName } =
+    (featuredImage && featuredImage.fields.file) || {};
+
+  console.log(featuredImage);
 
   return (
     <div className="post-blog">
       <h2 className="title">{name}</h2>
-      {featuredImage && (
+      {url && contentType === "image/jpeg" ? (
         <img
           className="featuredImage"
           src={featuredImage.fields.file.url}
           alt={name}
           title={name}
         />
+      ) : (
+        <iframe
+          type="text/html"
+          src={`https://www.youtube.com/embed/${
+            fileName.split("=")[fileName.split("=").length - 1]
+          }`}
+          style={{
+            border: "none",
+            width: "100%",
+            height: "650px",
+          }}
+        ></iframe>
       )}
       <section
         style={{
