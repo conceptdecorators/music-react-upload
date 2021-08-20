0b2d56757713192866c9adffb6666a4875a40170
conceptdecorators <conceptdecorators@email.com>
2021-08-20 15:48:18 -0400

    disable react-snap for now

--- src/components/layout/Blog.js
+++ src/components/layout/Blog.js
@@ -1,50 +1,45 @@
 import React, { useEffect, useState } from "react";
-import { Link, Redirect } from "react-router-dom";
 
 import { client } from "../../client";
 import Posts from "../../components/blog/Posts";
 
 import "./Blog.css";
 
-class Blog extends React.Component {
-  state = {
-    articles: [],
-  };
+const Blog = () => {
+  const [articles, setArticles] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     client
       .getEntries()
       .then((response) => {
-        console.log(response);
-        this.setState({
-          articles: response.items,
-        });
+        console.log("yooo===>>", response);
+        setArticles(response.items);
       })
-      .catch(console.error);
-  }
+      .catch((err) => {
+        console.error("errr==>>>", err);
+      });
+  }, []);
 
-  render() {
-    return (
-      <div className="App">
-        {/* <div className="container"> */}
-        <header>
-          <div className="wrapper-blog">
-            <h1 className="blog__page__header">ZILAH MUSIC PUBLISHING NEWS</h1>
-          </div>
-        </header>
-        <main>
-          <div className="blog__page">
-            <div className="blogs">
-              <div className="wrapper-blog">
-                <Posts posts={this.state.articles} />
-              </div>
+  return (
+    <div className="App">
+      {/* <div className="container"> */}
+      <header>
+        <div className="wrapper-blog">
+          <h1 className="blog__page__header">ZILAH MUSIC PUBLISHING NEWS</h1>
+        </div>
+      </header>
+      <main>
+        <div className="blog__page">
+          <div className="blogs">
+            <div className="wrapper-blog">
+              <Posts posts={articles} />
             </div>
           </div>
-        </main>
-        {/* </div> */}
-      </div>
-    );
-  }
-}
+        </div>
+      </main>
+      {/* </div> */}
+    </div>
+  );
+};
 
 export default Blog;
