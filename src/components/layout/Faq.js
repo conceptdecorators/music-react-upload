0e2a4bf03b33d0822a455c51997d9bc83b05f9bf
conceptdecorators <conceptdecorators@email.com>
2021-12-20 13:59:11 -0500

    .mobile-margin 0px margin added the sk-faq__header to the faq page for questions

--- src/components/layout/Faq.js
+++ src/components/layout/Faq.js
@@ -9,9 +9,7 @@ const Faq = () => {
       <div className="main">
         <div className="container">
           <div className="sk-faq--light pt-pb-160">
-            <h3 className="text-white medium-text services-social pt-90">
-              Questions
-            </h3>
+            <h3 className="sk-faq__header">Questions</h3>
             <div className="wrapper">
               {/* <!-- Accordion Heading One --> */}
               <div className="parent-tab">
